6c639481a8a9518bbe09bec2bcd8667794652b1d
HNurSelamet <haticenurselamet@gmail.com>
2022-08-16 22:28:18 +0300

    hipotenüs hesabı

---
+++ src/dikucgenhesabi/dikucgen.java
@@ -0,0 +1,25 @@
+package dikucgenhesabi;
+
+import java.util.Scanner;
+
+public class dikucgen {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("a kenarını giriniz : ");
+        int a = inp.nextInt();
+
+        System.out.print("b kenarını giriniz : " );
+        int b = inp.nextInt();
+
+        System.out.println("Hipotenüs uzunluğu : ");
+        double c = Math.sqrt ((a * a ) + (b * b));
+        System.out.print(c);
+
+
+
+
+
+
+
+    }
+}
