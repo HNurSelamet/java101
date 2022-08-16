71b2fa8622f33777e87546e657475f9aba393264
HNurSelamet <haticenurselamet@gmail.com>
2022-08-16 17:47:52 +0300

    KDV HESABI

---
+++ src/KDVhesaplama/KdvHesabi.java
@@ -0,0 +1,17 @@
+package KDVhesaplama;
+import java.util.Scanner;
+
+public class KdvHesabi {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.println("KDV'siz fiyatı giriniz : ");
+        int fiyat = inp.nextInt();
+
+        double kdv = fiyat <=1000 ? 0.18 : 0.08;
+        double kdvtutari = fiyat*kdv;
+        double kdvlifiyat = fiyat + kdvtutari;
+        System.out.println(kdvlifiyat);
+
+
+    }
+}
